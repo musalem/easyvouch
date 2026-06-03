f0a22b891838b0a9356c9de6768f3ceac4a7f131
Mustafa Shaheen <68672770+musalem@users.noreply.github.com>
2026-06-03 13:29:51 +0300

    Update DOWNLOAD_LINKS for mini version and URL
    Updated the download link and version for 'mini'.

--- downloadlinks.js
+++ downloadlinks.js
@@ -1,7 +1,7 @@
 const DOWNLOAD_LINKS = {
     mini: {
-        url: "https://bit.ly/4cCqWRZ",
-        version: "1.13"
+        url: "https://bit.ly/42LPNgF",
+        version: "1.16"
     },
     lite: {
         url: "https://bit.ly/48jeQKO",
