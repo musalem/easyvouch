9223c1f17b237b7a5f83f24872e09bc9911c21af
Mustafa Shaheen <68672770+musalem@users.noreply.github.com>
2026-09-22 23:11:32 +0300

    Change download links to new Dropbox URLs
    Updated download links for mini and lite versions.

--- downloadlinks.js
+++ downloadlinks.js
@@ -1,10 +1,10 @@
 const DOWNLOAD_LINKS = {
     mini: {
-        url: "https://bit.ly/3V1QmCo",
+        url: "https://www.dropbox.com/scl/fi/rmblf0w3ja742oez9wgep/EasyVouch.Mini.v1.21.zip?rlkey=vkhcyuidw023ajntihdzojgd8&st=cvb5lzvr&dl=1",
         version: "1.21"
     },
     lite: {
-        url: "https://bit.ly/48jeQKO",
+        url: "https://www.dropbox.com/scl/fi/y0ff3u287gzh8up9ibaei/EasyVouch-Lite-v1.1-Release.zip?rlkey=v60r7cu3opdf7s1ol1mv2vl71&st=lyz22j4o&dl=1",
         version: "1.1"
     }
 };
